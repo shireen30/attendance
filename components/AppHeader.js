300a4e86c273c387f9d97334c999f1833d96af26
shireen30 <70129608+shireen30@users.noreply.github.com>
2021-02-03 19:22:12 +0530

    Add files via upload

---
+++ components/AppHeader.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+export default class AppHeader extends React.Component {
+  render() {
+    return (
+      <View style={styles.viewStyle}>
+        <Text style={styles.textStyle}>SCHOOL ATTENDANCE APP</Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  viewStyle: {
+    backgroundColor: 'chocolate',
+    alignItems: 'center',
+    marginBottom: 30,
+  },
+  textStyle: {
+    fontSize:23,
+    fontWeight:'bold',
+  },
+});
